da6d4408aba3e0f29bed6ae8d71d2c8712c1da3d
Miguelangel Fernandez Mendoza <miguelangelxfm@gmail.com>
2018-09-10 17:55:19 +0200

    Make -l and -r into boolean flags

--- gitkeep.py
+++ gitkeep.py
@@ -1,15 +1,17 @@
 import click
 
 @click.command()
-@click.option('--recursive', '-r', default=False,
-	help='Whether to do this recursively for all sub-directories in the path')
-@click.option('--letgo', '-l', default=False,
-	help='Whether to remove the .gitkeep file instead of adding it.')
+@click.option('--recursive', '-r', default=False, is_flag=True,
+	help='Add/remove the .gitkeep files recursively for all sub-directories in the specified path.')
+@click.option('--letgo', '-l', default=False, is_flag=True,
+	help='Removes the .gitkeep files from the specified path.')
 @click.argument('path')
 def gitkeep(recursive, letgo, path):
-	"""A command line utility that adds .gitkeep files to directories in order
-	to force them into a Git repo even if they're empty.\n
+	"""Add a .gitkeep file to a directory in order
+	to push them into a Git repo even if they're empty.\n
 	Read more about why this is necessary at:
 	https://git.wiki.kernel.org/index.php/Git_FAQ#Can_I_add_empty_directories.3F"""
 
+	click.echo('recursive: %r' % recursive)
+	click.echo('Let go: %r' % letgo)
 	click.echo('Path: %s' % path)
