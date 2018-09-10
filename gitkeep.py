22a5e7357184a5287aed8aacaef609fa7101a50f
Miguelangel Fernandez Mendoza <miguelangelxfm@gmail.com>
2018-09-11 00:20:06 +0200

    Replace single quotes with double quotes

--- gitkeep.py
+++ gitkeep.py
@@ -3,20 +3,20 @@ import os
 import time
 
 @click.command()
-@click.option('--recursive', '-r', default=False, is_flag=True,
-	help='Add/remove the .gitkeep files recursively for all sub-directories in the specified path.')
-@click.option('--letgo', '-l', default=False, is_flag=True,
-	help='Removes the .gitkeep files from the specified path.')
-@click.argument('path')
+@click.option("--recursive", "-r", default=False, is_flag=True,
+	help="Add or remove the .gitkeep files recursively for all sub-directories in the specified path.")
+@click.option("--letgo", "-l", default=False, is_flag=True,
+	help="Remove the .gitkeep files from the specified path.")
+@click.argument("path")
 def gitkeep(recursive, letgo, path):
 	"""Add a .gitkeep file to a directory in order
-	to push them into a Git repo even if they're empty.\n
+	to push them into a Git repo even if they"re empty.\n
 	Read more about why this is necessary at:
 	https://git.wiki.kernel.org/index.php/Git_FAQ#Can_I_add_empty_directories.3F"""
 
-	click.echo('recursive: %r' % recursive)
-	click.echo('Let go: %r' % letgo)
-	click.echo('Path: %s' % path)
+	click.echo("recursive: %r" % recursive)
+	click.echo("Let go: %r" % letgo)
+	click.echo("Path: %s" % path)
 
 	if(os.path.exists(path)):
 		write_gitkeep(path)
@@ -24,7 +24,7 @@ def gitkeep(recursive, letgo, path):
 		click.echo("Path does NOT exist!")
 
 def write_gitkeep(path):
-	file = open(path + '.gitkeep', 'w')
+	file = open(path + ".gitkeep", "w")
 	file.write("Created %s\n" % time.strftime("%Y-%m-%d %H:%M:%S"))
 	file.write("By https://github.com/mig82/py-gitkeep\n\n")
 	file.write(".gitkeep files are a cool hack to push empty directories to Git.")
