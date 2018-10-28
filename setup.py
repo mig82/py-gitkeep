55764cfa26587aef66479a24cc60429469a277a3
Miguelangel Fernandez Mendoza <miguelangelxfm@gmail.com>
2018-10-28 20:57:57 +0100

    Document package for publication

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 from setuptools import setup
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+
 
 setup(
     name='gitkeep',
     version='0.1',
+	description="A tiny utility to force empty directories into a Git repo.",
+	long_description='README',
+	url='https://github.com/mig82/py-gitkeep',
+	author='Miguelangel Fernandez',
+	author_email='miguelangelxfm@gmail.com',
     py_modules=['gitkeep'],
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python :: 3.7',
+		'Topic :: Software Development :: Version Control :: Git',
+		'Topic :: Utilities'
+	],
     install_requires=[
         'Click',
     ],
